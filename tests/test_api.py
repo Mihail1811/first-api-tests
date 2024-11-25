cc00bab4e23b7978ff3f1df0079e769f1403dacb
Mishanya <mihailziganov1811@gmail.com>
2024-11-25 19:11:55 +0400

    fixed bugs

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,22 +5,25 @@ class TestID:
 
     def test_first(self):
         test_case = TestCase()
-        id_new_animal = test_case.create_animal('Orlock')
-        id_find_animal = test_case.find_animal(id_new_animal)['id']
-        assert id_new_animal == id_find_animal, (
+        new_animal = test_case.create_animal('Orlock')
+        id_find_animal = test_case.find_animal(new_animal)['id']
+        assert new_animal == id_find_animal, (
             "[FAILED]: Ids doesnt match {} and {}".format(
-                id_new_animal, id_find_animal))
-        id_delete_animal = test_case.delete_animal(id_new_animal)
+                new_animal, id_find_animal))
+        test_case.delete_animal(new_animal)
+        check_pet = test_case.find_animal(new_animal)
+        assert check_pet['message'] == "Pet not found", "Pet was not daleted!"
+
 
     def test_second(self):
         test_case = TestCase()
         id_pet = test_case.create_animal('Дружок')
         id_check_pet = test_case.check_pet(id_pet)['id']
+        assert id_pet == id_check_pet, "Ids don't match"
         check_pet = test_case.check_pet(id_pet)
-        id_delete_pet = test_case.delete_pet(id_pet)
-        check_after_delete = test_case.check_pet(id_pet)
+        test_case.delete_pet(id_pet)
         status = ['available', 'pending', 'sold']
         for i in status:
-            pets = test_case.find_by_status(i)
+            pets = test_case.find_pets_by_status(i)
             assert check_pet not in pets, \
-                f'ID {check_pet} присутствует в списке'
+                f"ID {check_pet} присутствует в списке"
--- tests_class.py
+++ tests_class.py
@@ -46,11 +46,21 @@ class TestCase:
         return self.response.json()
 
     def delete_animal(self, animal_id: str) -> dict:
+        """
+            Удаление животного по его ID через API-запрос.
+            :param animal_id: ID животного
+            :return: Данные, содержащие информацию об успехе или ошибке.
+        """
         url = 'pet/' + str(animal_id)
         self.response = self.request.send_request('delete', url)
         return self.response.json()
 
     def create_pet(self, name: str = 'Друг') -> str:
+        """
+            Создание нового животного через API-запрос
+            :param name: Имя животного, по умолчанию - Olaf
+            :return: id созданного животного
+        """
         data = {
                        "id": 337,
                        "category": {
@@ -74,16 +84,31 @@ class TestCase:
         return self.ID
 
     def check_pet(self, pet_id: str) -> dict:
+        """
+            Метод ищет животное по id
+            :param pet_id: id животного
+            :return: информация о животном
+        """
         url = 'pet/' + str(pet_id)
         self.response = self.request.send_request('get', url)
         return self.response.json()
 
     def delete_pet(self, pet_id: str) -> dict:
+        """
+            Удаление животного по его ID через API-запрос.
+            :param pet_id: ID животного
+            :return: Данные, содержащие информацию об успехе или ошибке.
+        """
         url = 'pet/' + str(pet_id)
         self.response = self.request.send_request('delete', url)
         return self.response.json()
 
-    def find_by_status(self, status: str):
+    def find_pets_by_status(self, status: str) -> dict:
+        """
+            Получение списка животных по указанному статусу через API-запрос.
+            :param status: Статус животного
+            :return: Список данных о всех животных с указанным статусом.
+        """
         url = f'pet/findByStatus?status={status}'
         self.response = self.request.send_request('get', url)
         return self.response.json()
